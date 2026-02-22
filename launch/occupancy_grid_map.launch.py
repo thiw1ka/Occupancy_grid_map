7b8511705d95779c64ab5603fc2d0b9c28f64295
rperera <rperera@ele.uri.edu>
2026-02-22 07:31:00 -0500

    Launch file works

--- launch/occupancy_grid_map.launch.py
+++ launch/occupancy_grid_map.launch.py
@@ -1,16 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
     """Launch occupancy grid map node and Gazebo."""
     
-    # Gazebo world launch - empty world
-    gazebo = ExecuteProcess(
-        cmd=['gazebo', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
-        output='screen'
+    # Launch TurtleBot3 empty world (this will start Gazebo)
+    turtlebot3_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('turtlebot3_gazebo'), 'launch', 'empty_world.launch.py'
+        ))
     )
     
     occupancy_grid_node = Node(
@@ -18,23 +21,23 @@ def generate_launch_description():
         executable='occupancy_grid_node',
         name='occupancy_grid_map_node',
         output='screen',
+        # arguments=['--ros-args', '--log-level', 'DEBUG'],
     )
 
-        
-    # visulizer = Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     arguments=['-d', os.path.join(
-    #         get_package_share_directory('slam_sim'),'rviz')+'/rviz1.rviz']
-    # )
-
-    #     simulation = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource([os.path.join(
-    #         get_package_share_directory('turtlebot3_gazebo'),'launch'),
-    #         '/turtlebot3_house.launch.py']),launch_arguments={}.items()
-    #     )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        # do not set a fixed node name to avoid name conflicts
+        output='screen',
+        # arguments=['--ros-args', '--log-level', 'DEBUG'],
+    )
 
     return LaunchDescription([
-        gazebo,
+        # export TurtleBot3 model for included launch
+        SetEnvironmentVariable('TURTLEBOT3_MODEL', 'burger'),
+        # ensure rviz has access to the X display from the current environment
+        SetEnvironmentVariable('DISPLAY', os.environ.get('DISPLAY', ':0')),
+        turtlebot3_launch,
         occupancy_grid_node,
+        rviz_node,
     ])
